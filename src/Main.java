14907952b5f0b364596aedb07389e3d809399069
Kshydlo <zikdanbar@gmail.com>
2026-06-01 20:27:55 +0900

    Задание №4

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
 
-        System.out.println("Собака = " + (dog - 3.5) + "\nКошка = " + (cat - 1.6) + "\nБумага = " + (paper - 7639));
+        var friend = 19;
+        System.out.println(friend);
+
+        friend = friend + 2;
+        System.out.println(friend);
+
+        friend = friend / 7;
+        System.out.println(friend);
     }
 }
