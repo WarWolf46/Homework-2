10a7b1b56691d911c4b869e908be86c9585a181f
Kshydlo <zikdanbar@gmail.com>
2026-06-01 20:07:40 +0900

    Задание №3

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,6 @@ public class Main {
         var cat = 3.6;
         var paper = 763789;
 
-        System.out.println("Собака = " + dog + "\nКошка = " + cat + "\nБумага = " + paper);
+        System.out.println("Собака = " + (dog - 3.5) + "\nКошка = " + (cat - 1.6) + "\nБумага = " + (paper - 7639));
     }
 }
