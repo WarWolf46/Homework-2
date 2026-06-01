77289a34c150be0359c3d3b1932440316990c520
Kshydlo <zikdanbar@gmail.com>
2026-06-01 16:49:12 +0900

    Задание №1

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println("Собака = " + dog + "\nКошка = " + cat + "\nБумага = " + paper);
+    }
+}
