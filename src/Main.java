f38d2e113268fde7d6d9d146840a1202d092c586
Kshydlo <zikdanbar@gmail.com>
2026-06-03 23:43:12 +0900

    Полное Д/З

--- src/Main.java
+++ src/Main.java
@@ -7,15 +7,15 @@ public class Main {
 
         System.out.println("Собака = " + dog + "\nКошка = " + cat + "\nБумага = " + paper);
         // Задача 2
-                var dog = 8.0;
-                var cat = 3.6;
-                var paper = 763789;
+                var Dog = 8.0;
+                var Cat = 3.6;
+                var Paper = 763789;
 
                 System.out.println("Собака = " + dog + 4 + "\nКошка = " + cat + 4 + "\nБумага = " + (paper + 4));
         // Задача 3
-        var dog = 8.0;
-        var cat = 3.6;
-        var paper = 763789;
+        var doG = 8.0;
+        var caT = 3.6;
+        var papeR = 763789;
 
         System.out.println("Собака = " + (dog - 3.5) + "\nКошка = " + (cat - 1.6) + "\nБумага = " + (paper - 7639));
         // Задача 4
@@ -49,10 +49,10 @@ public class Main {
         var weightDifference = boxerTwo - boxerOne;
         System.out.println("Разница между массами бойцов: " + weightDifference + " кг");
         // Задача 7
-        var boxerOne = 78.2;
-        var boxerTwo = 82.7;
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
 
-        var totalWeight = boxerOne + boxerTwo;
+        var TotalWeight = boxerOne + boxerTwo;
         System.out.println("Общая масса бойцов: " + totalWeight + " кг");
 
         var weightDivide = boxerTwo % boxerOne;
@@ -65,10 +65,10 @@ public class Main {
 
         System.out.println("Всего работников в компании - " + employees + " человек");
         // Задача 8.2
-        var totalHours = 640;
-        var workHoursPerEmployee = 8;
+        var totalWorkHours = 640;
+        var workHoursPerOneEmployee = 8;
 
-        var employees = totalHours / workHoursPerEmployee;
+        var Employees = totalHours / workHoursPerEmployee;
 
         employees = employees + 94;
 
